65c563ee3584fc31223e5f161c65800b2fa79275
zzt <873421427@qq.com>
2018-07-31 20:43:54 +0800

    增加后端路由

--- routes/studentsRoutes.js
+++ routes/studentsRoutes.js
@@ -7,6 +7,18 @@ router.post('/query', studentsController.getStudentsInfo);
 
 router.post('/insert/basicInfo', studentsController.addBasicInfo);
 
+router.post('/insert/family',studentsController.addFamily);
+
+router.post('/insert/cadre',studentsController.addCadre);
+
+router.post('/insert/award',studentsController.addAward);
+
+router.post('/insert/paper',studentsController.addPaper);
+
+router.post('/insert/patent',studentsController.addPatent);
+
+router.post('/insert/techProject',studentsController.addTechProject);
+
 
 
 module.exports = router;
