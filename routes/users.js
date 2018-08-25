b412980de6f06b09087e9f64d0cb1138ba356ec8
zzt <873421427@qq.com>
2018-08-25 22:36:47 +0800

    update user routers

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var router = express.Router();
+var router_before_login = express.Router();
+var router_after_login = express.Router();
 var userController = require('../controllers/user-controller.js');
 
 /* GET users listing. */
@@ -7,6 +8,15 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/register',userController.register);
+router_before_login.post('/register',userController.register);
 
-module.exports = router;
+router_before_login.get('/session',userController.checkSession);
+
+router_before_login.post('/login',userController.login);
+
+router_after_login.get('/logout',userController.logout)
+
+module.exports = {
+  beforeLogin: router_before_login,
+  afterLogin: router_after_login
+};
