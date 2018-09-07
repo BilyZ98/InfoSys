ef12549c00dd15733df581d3ba9314645827b46d
zzt <873421427@qq.com>
2018-09-07 10:48:35 +0800

    .

--- routes/notice.js
+++ routes/notice.js
@@ -6,3 +6,6 @@ const noticeController = require('../controllers/notice-controller.js')
 router.post('/addNotice',noticeController.addNotice);
 
 router.get('/getNotice', noticeController.getNotice);
+
+
+module.exports = router
